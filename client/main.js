b226a7e6157bca34e77056b5710d2113d9495bfc
Abdelrhman <abdelrhman_elsayed78@yahoo.com>
2017-01-05 05:33:25 -0800

    display images using each loop and with helper function

--- client/main.js
+++ client/main.js
@@ -1,22 +1,51 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+// import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(5);
+// });
+
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
+
+
+var image_data = [
+	{
+		img_src:"1.jpg",
+		img_alt:"Red Converse"
+	},
+	{
+		img_src:"2.jpg",
+		img_alt:"Black Converse"
+	},
+	{
+		img_src:"3.jpg",
+		img_alt:"Converse"
+	},
+	{
+		img_src:"4.jpg",
+		img_alt:"Converse"
+	},
+	{
+		img_src:"5.jpg",
+		img_alt:"Converse"
+	}
+];
+
+Template.images.helpers({images:image_data});
