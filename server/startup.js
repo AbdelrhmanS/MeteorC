4d9a5f67bb390e318b3507d45dd965eb3fd8bae1
Abdelrhman <abdelrhman_elsayed78@yahoo.com>
2017-01-08 03:40:02 -0800

    add mongodb collection with images and add delete btn with his functionlity and add rating system with sorting grid based on rating value

---
+++ server/startup.js
@@ -0,0 +1,21 @@
+Images = new Mongo.Collection("images");
+
+console.log(Meteor.isServer);
+
+if(Meteor.isServer){
+	console.log("I'm a server");
+	console.log(Images.find().count());
+	Meteor.startup(function(){
+		if(Images.find().count() == 0){
+			for(var i = 1 ; i <= 5 ;i++)
+			{
+				Images.insert(
+					{
+						img_src: i+".jpg",
+						img_alt:"Red Converse"
+					}
+				);
+			}
+		}
+	});
+}
